40ac916d004af2cae869d01d928c74b83e8d8285
Himanshu Gupta <himangupta04@gmail.com>
2023-01-30 11:39:39 +0530

    first commit

---
+++ hyp.py
@@ -0,0 +1,9 @@
+p = 12
+base = 5
+
+hyp =(p**2 + base**2)**.5
+print("PYTHAGOREAM THEOREM")
+print(f"Perpendicular is {p}")
+print(f"Base is {base}")
+print('=='*10)
+print("Hypotenuse is",hyp)
---
+++ si.py
@@ -0,0 +1,11 @@
+# SI Calculator
+p = 1000
+r = .8
+t = 10
+
+s = p*r*t/100
+print(f'Principal is ${p}')
+print(f'Rate is {r}%')
+print(f'Time is {t}yrs')
+print('---'*10)
+print(f'Simple Intrest is {s}')
